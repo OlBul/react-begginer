eb6cb7aa8fbf25064065b1ef990cd7fe6d6da81d
OlBul <olhabulhakova@gmail.com>
2022-12-02 14:48:43 +0200

    currency-convertor

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+import { Block } from './Block';
 import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
 
-// Тут список пользователей: https://reqres.in/api/users
+//https://cdn.cur.su/api/latest.json
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [invites, setInvites] = useState([])
-  const [isLoading, setLoading] = useState(true)
-  const [success, setSuccess] = useState(false)
-  const [searchValue, setSearchValue] = useState('')
+  const [fromCurrency, setFromCurrency] = useState('RUB')
+  const [toCurrency, setToCurrency] = useState('USD')
+  const [fromPrice, setFromPrice] = useState(0)
+  const [toPrice, setToPrice] = useState(1)
+ // const [rates, setRates] = useState({})
+  const ratesRef = useRef({})
+
+  const onChangeFromPrice = (value) => {
+    const price = value / ratesRef.current[fromCurrency]
+    const result = price * ratesRef.current[toCurrency]
+    setToPrice(result.toFixed(3))
+    setFromPrice(value)
+ }
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value
+    setFromPrice(result.toFixed(3))
+    setToPrice(value)
+ }
 
   useEffect(() => {
-    fetch('https://reqres.in/api/users').then(res => res.json()).then(json => {
-      setUsers(json.data)
+    onChangeFromPrice(fromPrice)
+  },[fromCurrency])
+  
+  useEffect(() => {
+    onChangeToPrice(toPrice)
+  },[toCurrency])
+  
+  useEffect(() => {
+    fetch("https://cdn.cur.su/api/latest.json")
+      .then((res) => res.json())
+      .then((json) => {
+        ratesRef.current= json.rates
+        onChangeToPrice(1)
     }).catch(err => {
       console.warn(err)
-      alert('Ошибка при получении данных о пользователях')
-    }).finally(() => setLoading(false))
-  }, [])
-  
-  const onChangeSearchValue = (e) => {
-  setSearchValue(e.target.value)
-  }
-  
-  const onClickInvite = (id) => {
-    if (invites.includes(id)) {
-      setInvites(prev => prev.filter(_id =>_id !== id))
-    } else {
-      setInvites((prev) => [...prev, id])
-    }
-  }
-
-  const onClickSendInvites = () => {
-    setSuccess(true)
-  }
+      alert('Не удалось получить данные!')
+})
+  },[])
 
   return (
     <div className="App">
-      {
-        success ? <Success count={ invites.length} /> :
-          <Users onChangeSearchValue={onChangeSearchValue} searchValue={searchValue} items={users} isLoading={isLoading} invites={invites} onClickInvite={onClickInvite} onClickSendInvites={ onClickSendInvites} />
-      }     
+      <Block value={fromPrice} currency={fromCurrency} onChangeCurrency={setFromCurrency} onChangeValue={onChangeFromPrice } />
+      <Block value={toPrice} currency={toCurrency} onChangeCurrency={setToCurrency} onChangeValue={onChangeToPrice }/>
     </div>
   );
 }
