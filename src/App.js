32fff78c9035ace1040c068c497b641f3c624ab7
OlBul <olhabulhakova@gmail.com>
2022-12-04 14:03:41 +0200

    add key

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
       </div>
       <ul className="pagination">
         {[...Array(5)].map((_, i) => (
-          <li onClick={() => setPage(i + 1)} className={page === (i + 1)  ? 'active' : ''}>{ i + 1}</li>
+          <li key={i} onClick={() => setPage(i + 1)} className={page === (i + 1)  ? 'active' : ''}>{ i + 1}</li>
         ))}
       </ul>
     </div>
